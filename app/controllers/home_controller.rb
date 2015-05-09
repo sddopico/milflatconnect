f21b2d98eb38988e57941fae8d8b2cfa603161ed
yaboyanees <a.merzi@yahoo.com>
2015-05-08 21:26:12 -0500

    associated comments with flat listing and added a link to each flat listing from home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
+  	@flats = Flat.all
+  	@comments = Comment.all
+  	@users = User.all
+  end
+  def show
+  	@flats = Flat.all
+  	@comments = Comment.all
+  	@users = User.all
   end
 end
